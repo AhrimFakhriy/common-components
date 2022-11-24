baa93d858548d777ffd1239f33531198183f05db
Ahrim Fakhriy <ahrim45@gmail.com>
2022-11-24 17:11:33 +0800

    initial commit

--- src/CommonComponentsServiceProvider.php
+++ src/CommonComponentsServiceProvider.php
@@ -4,12 +4,11 @@ namespace AhrimFakhriy\CommonComponents;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use AhrimFakhriy\CommonComponents\Commands\CommonComponentsCommand;
 
 class CommonComponentsServiceProvider extends PackageServiceProvider
 {
 
-    protected final string $packageName = 'common-components';
+    public string $packageName = 'common-components';
 
     public function configurePackage(Package $package): void
     {
